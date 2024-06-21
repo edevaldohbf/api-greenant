8e62908d017e2409f9e8349475fa0749e0128d9b
Edevaldo <edevaldohbfilho@gmail.com>
2024-06-20 23:59:09 -0300

    feat(app files): exclude unecessary files
    exclude app.controller.spec, app.controller and app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
+import { DeviceMeasurementsModule } from './device-measurements/device-measurements.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DeviceMeasurementsModule],
+  providers: [DatabaseService],
 })
 export class AppModule {}
